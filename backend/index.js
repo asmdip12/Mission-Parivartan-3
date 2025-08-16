35ff81f45529f046c206b012e36292bb543789ae
asmdip12 <dg013555@gmail.com>
2025-08-17 00:10:41 +0530

    final commit4

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors({
-  origin: "https://mission-parivartan3.vercel.app",
+  origin: "https://mission-parivartan3-fxl8mvs9m-asmdip12s-projects.vercel.app/",
   credentials: true
 }));
 app.use(express.json());
